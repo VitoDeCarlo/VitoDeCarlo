2ef29c019d052b8093b4de6b1e85b567c992648b
Vito DeCarlo <vito@vitodecarlo.com>
2021-09-14 21:14:40 -0400

    Initial Push after GitHub reorg.

---
+++ VitoDeCarlo.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "VitoDeCarlo.Theme",
+    Author = "thePulse Inc.",
+    Website = "https://thepulse.net",
+    Version = "0.0.1",
+    Description = "OrchardCore CMS Theme for Vito DeCarlo's personal site."
+)]
