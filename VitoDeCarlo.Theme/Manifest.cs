13087849dd8462af32da2dfebfdaf1fa3788c521
Vito DeCarlo <vito@vitodecarlo.com>
2021-09-17 13:05:31 -0400

    Removing old solution to prep for Blazor solution.

--- VitoDeCarlo.Theme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "VitoDeCarlo.Theme",
-    Author = "thePulse Inc.",
-    Website = "https://thepulse.net",
-    Version = "0.0.1",
-    Description = "OrchardCore CMS Theme for Vito DeCarlo's personal site."
-)]
