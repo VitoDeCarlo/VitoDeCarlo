093a4e891d318de9582ddbe5775acaddc3ac7407
Vito DeCarlo <vito@vitodecarlo.com>
2021-09-19 11:24:16 -0400

    Setup service worker and manifest to allow install as a Progressive Web App.

--- VitoDeCarlo.Blazor/Pages/Video.razor.js
+++ VitoDeCarlo.Blazor/Pages/Video.razor.js
@@ -1,4 +1,12 @@
-﻿// This function creates an <iframe> (and YouTube player) after the API code downloads.
+﻿// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+// This function creates an <iframe> (and YouTube player) after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('existing-iframe-example', {
